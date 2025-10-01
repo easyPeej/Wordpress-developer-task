3a256a2f58e8502ab603f392a484681435aca710
PJ Scott-Caven <pscottcaven@gmail.com>
2025-10-01 10:54:05 +0100

    style: minor tweaks

--- wp-content/themes/blankslate/page.php
+++ wp-content/themes/blankslate/page.php
@@ -4,20 +4,20 @@
             <header class="header">
                 <h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
             </header>
-            <div class="entry-content" itemprop="mainContentOfPage">
-                <?php if (has_post_thumbnail()) {
-                    the_post_thumbnail('full', array('itemprop' => 'image'));
-                } ?>
-                <section class="page-body">
+            <main id="content">
+                <div class="entry-content" itemprop="mainContentOfPage">
+                    <?php if (has_post_thumbnail()) {
+                        the_post_thumbnail('full', array('itemprop' => 'image'));
+                    } ?>
                     <?php the_content(); ?>
-                </section>
-                <?php #the_content(); ?>
-                <div class="entry-links"><?php wp_link_pages(); ?></div>
-            </div>
+                    <div class="entry-links"><?php wp_link_pages(); ?></div>
+                </div>
         </article>
         <?php if (comments_open() && !post_password_required()) {
             comments_template('', true);
         } ?>
 <?php endwhile;
 endif; ?>
+</main>
+
 <?php get_footer(); ?>
